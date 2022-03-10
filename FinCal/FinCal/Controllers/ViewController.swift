be13147e98d9e789fa56575f664ab5e51e4f77be
dinuka-rp <drpiyadigama@gmail.com>
2022-03-10 15:04:03 +0530

    create initial entry views

---
+++ FinCal/FinCal/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinCal
+//
+//  Created by Dinuka Piyadigama on 2022-03-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
